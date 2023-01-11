1e2dbc693fc37674c40d4912543a154e633e5cb4
AndrewValenzuela20 <andyvalenzuela864@gmail.com>
2023-01-10 20:20:58 -0600

    v3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("qwerty");
+        System.out.println("qwe;lkjnrg;elsknjmtg';owelrlktkjtjtgrty");
     }
 }
